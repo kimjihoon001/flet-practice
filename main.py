60fd41b030a5705ff6de7280a5b85f8011409e0b
김지훈 <kimjihoon001@gmail.com>
2026-03-12 16:42:49 +0900

    260312

--- main.py
+++ main.py
@@ -1,84 +1,33 @@
-from dataclasses import field
-
 import flet as ft
+from basic_calc import CalculatorApp as BasicCalculator
+from engineering_calc import CalculatorApp as EngineeringCalculator
 
 
 def main(page: ft.Page):
-    page.title = "Calc App"
-    result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
 
-    @ft.control
-    class CalcButton(ft.Button):
-        expand: int = field(default_factory=lambda: 1)
+    page.window.resizable = False
 
-    @ft.control
-    class DigitButton(CalcButton):
-        bgcolor: ft.Colors = ft.Colors.WHITE_24
-        color: ft.Colors = ft.Colors.WHITE
+    def show_basic(e=None):
+        page.controls.clear()
 
-    @ft.control
-    class ActionButton(CalcButton):
-        bgcolor: ft.Colors = ft.Colors.ORANGE
-        color: ft.Colors = ft.Colors.WHITE
+        # 일반 계산기 창 크기
+        page.window.width = 380
+        page.window.height = 380
 
-    @ft.control
-    class ExtraActionButton(CalcButton):
-        bgcolor: ft.Colors = ft.Colors.BLUE_GREY_100
-        color: ft.Colors = ft.Colors.BLACK
+        page.add(BasicCalculator(show_engineering))
+        page.update()
 
-    page.add(
-        ft.Container(
-            width=350,
-            bgcolor=ft.Colors.BLACK,
-            border_radius=ft.BorderRadius.all(20),
-            padding=20,
-            content=ft.Column(
-                controls=[
-                    ft.Row(controls=[result], alignment=ft.MainAxisAlignment.END),
-                    ft.Row(
-                        controls=[
-                            ExtraActionButton(content="AC"),
-                            ExtraActionButton(content="+/-"),
-                            ExtraActionButton(content="%"),
-                            ActionButton(content="/"),
-                        ]
-                    ),
-                    ft.Row(
-                        controls=[
-                            DigitButton(content="7"),
-                            DigitButton(content="8"),
-                            DigitButton(content="9"),
-                            ActionButton(content="*"),
-                        ]
-                    ),
-                    ft.Row(
-                        controls=[
-                            DigitButton(content="4"),
-                            DigitButton(content="5"),
-                            DigitButton(content="6"),
-                            ActionButton(content="-"),
-                        ]
-                    ),
-                    ft.Row(
-                        controls=[
-                            DigitButton(content="1"),
-                            DigitButton(content="2"),
-                            DigitButton(content="3"),
-                            ActionButton(content="+"),
-                        ]
-                    ),
-                    ft.Row(
-                        controls=[
-                            DigitButton(content="0", expand=2),
-                            DigitButton(content="."),
-                            ActionButton(content="="),
-                        ],
-                    ),
-                ]
-            ),
-        )
-    )
+    def show_engineering(e=None):
+        page.controls.clear()
+
+        # 공학용 계산기 창 크기
+        page.window.width = 680
+        page.window.height = 380
+
+        page.add(EngineeringCalculator(show_basic))
+        page.update()
+
+    show_basic()
 
 
-if __name__ == "__main__":
-    ft.run(main)
+ft.app(target=main)
